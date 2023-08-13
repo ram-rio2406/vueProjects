3710a27795dc7632b5847a43a149454179a022e8
Ramchandar S R <ramchandar.sr@gmail.com>
2023-08-13 23:43:26 +0530

    Added:Vue Router

--- Bedtime-Tracker/bedtime-tracker/src/main.js
+++ Bedtime-Tracker/bedtime-tracker/src/main.js
@@ -1,15 +1,30 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { createRouter, createWebHashHistory } from 'vue-router';
+
 //vuetify
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+const routes = [
+	{
+		name: 'Home',
+		path: '/',
+		component: App
+	},
+]
+
+const router = createRouter({
+	routes,
+	history: createWebHashHistory()
+});
+
 const vuetify = createVuetify({
 	components,
 	directives,
 })
 
-createApp(App).use(vuetify).mount('#app')
+createApp(App).use(router).use(vuetify).mount('#app');
